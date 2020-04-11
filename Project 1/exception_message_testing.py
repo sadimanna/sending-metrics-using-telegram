9245d0e4865e7aad10b501211349ea539c18a67b
Siladittya Manna <sadimanna@users.noreply.github.com>
2020-04-12 02:15:28 +0530

    Add files via upload

---
+++ Project 1/exception_message_testing.py
@@ -0,0 +1,29 @@
+import requests
+import time
+
+access_token = '1094213176:AAFPM2lzoWH_fvQXgDjUtluJCctamMp5Ax0'
+
+class TGBot(object):
+	def __init__(self, access_token):
+		self.access_token = access_token
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/getUpdates'		
+		self.response = requests.get(self.ping_url).json()
+		self.chat_id = self.response['result'][0]['message']['chat']['id']
+		#return self.chat_id
+
+	def send_message(self,message):
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/sendMessage?'+\
+					'chat_id='+str(self.chat_id)+'&parse_mode=Markdown'+'&text='+message
+		self.response = requests.get(self.ping_url)
+
+if __name__ == '__main__':
+	bot = TGBot(access_token)
+	bot.send_message('Exception Message Testing!!')
+
+	try:
+		printf(1/0)
+	except Exception as ex:
+		template = "An exception of type {0} occurred.\nArguments:\n{1!r}"
+    	message = template.format(type(ex).__name__, ex.args)
+    	bot.send_message(message)
+	
---
+++ Project 1/telegram_bot.py
@@ -0,0 +1,49 @@
+import requests
+import time
+
+access_token = '1094213176:AAFPM2lzoWH_fvQXgDjUtluJCctamMp5Ax0'
+
+class TGBot(object):
+	def __init__(self, access_token):
+		self.access_token = access_token
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/getUpdates'		
+		self.response = requests.get(self.ping_url).json()
+		self.chat_id = self.response['result'][0]['message']['chat']['id']
+		self.number_of_messages = len(self.response['result'])
+		self.new_message = []
+		#return self.chat_id
+
+	def send_message(self,message):
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/sendMessage?'+\
+					'chat_id='+str(self.chat_id)+'&parse_mode=Markdown'+'&text='+message
+		self.response = requests.get(self.ping_url)
+
+	def send_photo(self,filepath):
+		file_ = open(filepath,"rb")
+		file_dict = {'photo':file_}
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/sendPhoto?'+\
+					'chat_id='+str(self.chat_id)
+		self.response = requests.post(self.ping_url,files=file_dict)
+		file_.close()
+
+	def check_new_message(self):
+		self.ping_url = 'https://api.telegram.org/bot'+str(self.access_token)+'/getUpdates'
+		self.response = requests.get(self.ping_url).json()
+		if len(self.response['result']) > self.number_of_messages:
+			for i in range(len(self.response['result'])-self.number_of_messages):
+				self.new_message.append(self.response['result'][self.number_of_messages+i]['message']['text'])
+			self.number_of_messages = len(self.response['result'])
+		print(self.new_message)
+
+if __name__ == '__main__':
+	bot = TGBot(access_token)
+	bot.send_message('Hurray!!')
+	#bot.send_photo('/mnt/d/bracelet2.JPG') 
+	stime = time.time()
+	while time.time()-stime<2:
+		print('Waiting...')
+		#bot.send_message('Hurray!!')
+		#bot.send_photo('telegraph.jpg') 
+	bot.check_new_message()
+
+	#Appropriate file name for windows needs to be given
